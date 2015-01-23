61b10cdc662ceeb77cff2a59fa47f97cebc53bf4
Venkat Tumkunta <venkateshwara.reddy.khammampadu@one.verizon.com>
2015-01-23 02:20:21 -0600

    added the code for the assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program contains 2 function, makeCacheMatrix & CacheSolve.
+## which help to reach data from matrix and return inverted matrix.
+## if the matrix is already present, it returns from Cache, else it is created and returned.
 
-## Write a short comment describing this function
-
+## This function created a necessary functions to set or get matrix.
+## Finally this function returns a matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+		m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(mat) m <<- mat
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
+###---------------------------------------------------------
 
-## Write a short comment describing this function
+
+## This metric returns a inverse of the a input metrix; if..
+## if the metrics is alreay present get it from Cache, 
+## else calculate and get it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m<-x$getmatrix()
+		if(!is.null(m)){
+			message("getting inverted matrix")
+			return(solve(m))
+		}
+		
+		mat <- x$get()
+		m <- solve(mat,...)
+		mat$setmatrix(m)
+		m
 }
+
+###---------------------------------------------------------
